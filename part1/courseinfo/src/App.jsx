5d664f0670faffb08a3746b1270105e4b991287f
Daembius <julienmayard@me.com>
2024-06-01 23:23:06 +0700

    Exercise 1.5: Course Information step 5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,5 +1,5 @@
 const Header = (props) => {
-  console.log(props)
+  // console.log(props)
   return (
     <div>
       <h1>{props.course}</h1>
@@ -8,7 +8,7 @@ const Header = (props) => {
 };
 
 const Part = (props) => {
-  console.log(props)
+  // console.log("Part - props: " +props)
   return (
     <p>
       {props.name}: <span>{props.exercises}</span>
@@ -46,28 +46,29 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };

d40be3154d869fb723834c6497698cb2f743b53d
Daembius <julienmayard@me.com>
2024-06-14 12:52:13 +0700

    Labs Part 1d - Update of the state is asynchronous

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,36 +1,45 @@
 import { useState } from "react";
 
-const History = (props) => {
-  if(props.allClicks.length === 0) {
-    return (
-      <div>
-        The app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      Button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
+// const History = (props) => {
+//   if(props.allClicks.length === 0) {
+//     return (
+//       <div>
+//         The app is used by pressing the buttons
+//       </div>
+//     )
+//   }
+//   return (
+//     <div>
+//       Button press history: {props.allClicks.join(' ')}
+//     </div>
+//   )
+// }
+
+// const Button = ({ handleClick, text }) => {
+//   <button onClick={handleClick}>
+//     {text}
+//   </button>
+// }
 
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
-
   const [allClicks, setAll] = useState([])
 
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right) 
   }
-
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
     setRight(right + 1)
+
+    setTotal(left + right)
   }
 
   return (
@@ -39,7 +48,9 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-      <History allClicks={allClicks} />
+      <p>{allClicks.join(' ')}</p>
+
+      <p>total {total}</p>
     </div>
   )
 }

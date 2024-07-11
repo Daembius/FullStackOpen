e09a1e2c99be448fe1043e6a268b30533eb8ff52
Daembius <julienmayard@me.com>
2024-07-12 00:51:52 +0700

    Exercises 2.2: Course information step 7 - first try

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,6 +1,7 @@
 const Header = ({ course }) => <h1>{course}</h1>;
 
-// const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
 
 const Part = ({ part }) => (
   <p>
@@ -21,7 +22,7 @@ const Course = ({ course }) => {
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
-      {/* <Total parts={course.parts} /> */}
+      <Total sum={course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
     </div>
   );
 };

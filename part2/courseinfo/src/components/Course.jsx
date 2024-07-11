13de76e8beee3ced14dbfe597ed386dc6b444d71
Daembius <julienmayard@me.com>
2024-07-11 15:37:14 +0700

    Exercise 2.1: Course information step 6

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,37 @@
+
+
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    <Part
+      part={parts[0]} 
+    />
+    <Part
+      part={parts[1]} 
+    />
+    <Part
+      part={parts[2]} 
+    />      
+  </>
+
+App
+  Course
+    Header
+    Content
+      Part
+      Part
+      ...
+
+const Course = ({course}) =>{
+
+}
+
+Export default Course
--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,38 +1,41 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App(props) {
-  const { notes } = props;
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => (
-          <li>{note.content}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-export default App;
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
 
 // const App = (props) => {
-//   const { notes } = props
+//   const { notes } = props;
 
 //   return (
 //     <div>
 //       <h1>Notes</h1>
 //       <ul>
-//         <li>{notes[0].content}</li>
-//         <li>{notes[1].content}</li>
-//         <li>{notes[2].content}</li>
+//         {notes.map((note) => (
+//           <li key={note.id}>
+//             {note.content}
+//           </li>
+//         ))}
 //       </ul>
 //     </div>
-//   )
-// }
+//   );
+// };
 
-// export default App
+// export default App;
+
+
+import Note from './components/Note'
+
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App

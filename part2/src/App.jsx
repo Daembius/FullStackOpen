9b5b08a56c2622442601ea158cfb625e7aeee963
Daembius <julienmayard@me.com>
2024-07-14 22:02:07 +0700

    Part 2 b - Saving the notes in the component state

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,31 +1,16 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
+import { useState } from 'react'
+import Note from './components/Note'
 // import "./App.css";
 
-// const App = (props) => {
-//   const { notes } = props;
-
-//   return (
-//     <div>
-//       <h1>Notes</h1>
-//       <ul>
-//         {notes.map((note) => (
-//           <li key={note.id}>
-//             {note.content}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default App;
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
 
 
-import Note from './components/Note'
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
 
-const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
@@ -34,8 +19,13 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+
+      <form onSubmit={addNote}>
+        <input />
+        <button type="submit">save</button>
+      </form>   
     </div>
   )
 }
 
-export default App
+export default App 

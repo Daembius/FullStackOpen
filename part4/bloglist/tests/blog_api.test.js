246f4d6948cdfc8d8afa70255a48206b4e3b4095
Daembius <julienmayard@me.com>
2025-01-03 00:50:44 +0700

    Completed exercise 4.10: Added tests for POSTing new blogs

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 const { test, after, before, beforeEach } = require('node:test')
-const assert = require('assert') // Include assert from Node.js assert module
+const assert = require('assert')
 const supertest = require('supertest')
-const app = require('../app') // Now pointing to app.js
+const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 const { blogs } = require('./test_helper')
@@ -31,11 +31,46 @@ test('blogs are returned as json and have id property', async () => {
   // Check if there are blogs in the response
   assert.strictEqual(response.body.length, blogs.length)
 
-  // Check if each blog has an id property
   response.body.forEach((blog) => {
     assert.ok(blog.id !== undefined, 'id should be defined')
     assert.ok(blog._id === undefined, '_id should not be present')
   })
+})
+
+test('a new blog can be added via POST', async () => {
+  const newBlog = {
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'test-url.com',
+    likes: 7,
+  }
+
+  const postResponse = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  // Here we use postResponse to check the returned data
+  const createdBlog = postResponse.body
+  assert.ok(
+    createdBlog.id !== undefined,
+    'id should be defined for the newly created blog'
+  )
+  assert.ok(
+    createdBlog._id === undefined,
+    '_id should not be present for the newly created blog'
+  )
+
+  const blogsInDb = await Blog.find({})
+  assert.strictEqual(blogsInDb.length, blogs.length + 1)
+
+  const addedBlog = blogsInDb.find((b) => b.title === 'Test Blog')
+  assert.ok(addedBlog)
+  assert.strictEqual(addedBlog.title, newBlog.title)
+  assert.strictEqual(addedBlog.author, newBlog.author)
+  assert.strictEqual(addedBlog.url, newBlog.url)
+  assert.strictEqual(addedBlog.likes, newBlog.likes)
 })
 
 after(async () => {
